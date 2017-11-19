41e1a15a7b592ab4b6c40feb6b92434e47966bd0
katkinso <katyratkinson@yahoo.com>
2017-11-19 10:08:15 -0800

    /GET todo by id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 
+const {ObjectID} = require('mongodb')
 var { mongoose } = require('./db/mongoose')
 var { Todo } = require('./models/todo')
 var { User } = require('./models/user')
@@ -27,6 +28,32 @@ app.get('/todos',(req,res) =>{
    },(e) => {
       res.status(400).send(e)
    })
+})
+
+//var id = '5a11ac8ee657a51f3d579e44'
+app.get('/todos/:id',(req,res) => {
+  // res.send(req.params) //get value from req get
+  //validate id using isvalidid is not stop and respond with 404 - send empty body
+  var id = req.params.id
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+    res.send({todo})
+
+  },(e) => {
+    res.status(400).send()
+  })
+
+  //findby id
+    //success - if todo send it back, if no todo then call succesed & not found then send back 404 & eptyy
+    //err - 400 & send empty body back
+
+
 })
 
 app.listen(3000, () => {
